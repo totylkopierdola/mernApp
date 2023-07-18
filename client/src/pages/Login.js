b1a89afe9b0e77c9dd5d9203c73ffd266826668e
Mateusz Kudraj <mkudr@softserveinc.com>
2023-07-18 13:29:30 +0200

    login and register structure

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import { Button, Card, Divider, Form, Input } from 'antd'
+import {Link} from 'react-router-dom'
+
 
 const Login = () => {
+  
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className='authentication'>
+      <Card className='authenticaction-form'>
+        <h1>Log in</h1>
+
+        <Divider />
+
+        <Form layout='vertical' onFinish={onFinish}>
+          
+          <Form.Item label='Email' name='email'>
+            <Input placeholder='email' type='email'/>
+          </Form.Item>
+
+          <Form.Item label='Password' name='password'>
+            <Input placeholder='password' type='password'/>
+          </Form.Item>
+
+          <Button className='mt-3' block type="primary" htmlType='submit'>Login</Button>
+
+          <Link className='d-flex w-100 mt-1 text-right justify-content-end small' to='/register'>Register now</Link>
+
+        </Form>
+      </Card>
+    </div>
+  );
 }
 
 export default Login
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import { Button, Card, Divider, Form, Input } from 'antd'
+import {Link} from 'react-router-dom'
+
 
 const Register = () => {
+
+  const onFinish = (values) => {
+    console.log(values)
+  }
+
   return (
     <div className='authentication'>
-      <div className='authenticaction-form'>
+      <Card className='authenticaction-form'>
         <h1>Register</h1>
-      </div>
+
+        <Divider />
+
+        <Form layout='vertical' onFinish={onFinish}>
+
+          <Form.Item label='Name' name='name'>
+            <Input placeholder='name' />
+          </Form.Item>
+
+          <Form.Item label='Email' name='email'>
+            <Input placeholder='email' type='email'/>
+          </Form.Item>
+
+          <Form.Item label='Password' name='password'>
+            <Input placeholder='password' type='password'/>
+          </Form.Item>
+
+          <Button className='mt-3' block type="primary" htmlType='submit'>Register</Button>
+
+          <Link className='d-flex w-100 mt-1 text-right justify-content-end small' to='/login'>Back to Login</Link>
+
+        </Form>
+      </Card>
     </div>
   )
 }
