dfad452d010c7c2d51391628c0f694b43338277e
Mateusz Kudraj <mkudr@softserveinc.com>
2023-07-18 12:29:09 +0200

    init general concept v2

---
+++ client/src/pages/Register.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div className='authentication'>
+      <div className='authenticaction-form'>
+        <h1>Register</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Register
